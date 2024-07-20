d1ab329897c0bfb8688d6d1966b65fce583fb50e
Ankith Rai <raiankith1@gmail.com>
2024-07-20 17:08:16 +0530

    add new files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import StockDashboard from './modules/StockDashboard'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <StockDashboard />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/modules/StockDashboard.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+interface StockDashboardProps {
+  symbol?: string;
+}
+
+const StockDashboard: React.FC<StockDashboardProps> = ({ symbol }) => {
+  return ''
+};
+
+export default StockDashboard;
