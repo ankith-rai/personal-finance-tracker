9e132f3a3dc00c322c7203dcff857539a6209847
Ankith Rai <raiankith1@gmail.com>
2025-05-16 14:06:23 +0530

    add login page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,9 @@ const App: React.FC = () => (
       <h1 className="text-2xl font-bold mb-4 text-center">Finance Tracker</h1>
       <Transactions />
     </div>
+    <div>
+      {/* TODO: Import and implement LoginPage component */}
+    </div>
   </ApolloProvider>
 );
 
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Transactions: React.FC = () => {
+  return <></>
+};
+
+export default Transactions;
